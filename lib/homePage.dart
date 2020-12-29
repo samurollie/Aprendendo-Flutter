4461f24a99b7038cb114fd62632f358ff6959893
samurollie <53665491+samurollie@users.noreply.github.com>
2020-12-29 03:49:06 -0300

    Scaffold

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -13,23 +13,45 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      //Estou retornando um container, um widget básico
-      child: Center(
+    return Scaffold(
+      // Scaffold é um tipo de Widget que possui todas as regras de estilo do Material Design, do android
+      appBar: AppBar(
+        // AppBar é uma propriedade do Scaffold que implementa automaticamente a barra superior do app
+        title: Center(
+          child: Text('Bem Vindo!'),
+        ),
+      ),
+      body: Center(
+        // Body tem tudo que estará no "corpo da página"
         // Centraliza o que estiver dentro dele
         child: GestureDetector(
           // O widget GestureDetector é um widget que detecta gestos
-          child: Text('Samuel Contador: $count'), // O texto
+          child: Text(
+            'Samuel Contador: $count',
+            style: TextStyle(
+              fontSize: 25,
+            ),
+          ), // O texto
           onTap: () {
             // OnTap é um atributo do widget GestureDetector, tudo que está dentro dele será executado toda vez q alguem tocar no widget
-            print('clicado'); //Imprime clicado no terminal (DART)
             setState(() {
               // Função que notifica ao Flutter que ele deve atualizar o estado do widget, é recomendável colocar dentro desta função, apenas o que vai acontecer pra atualizar efetivamente o estado do widget
               count++; // Aumenta o contador
             });
-            print('count: $count'); // Imprime o contador no terminal
           },
         ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        // FloatingAction Button implementa um botão flutuante (tipo o de postar status no whatsapp)
+        child:
+            Icon(Icons.add), // Estou usando um icone pra ficar dentro do botão
+        onPressed: () {
+          // Função que determina o que vai acontecer quando apertarmos o botão flutuante, neste caso, estamos fazendo o mesmo do Gesture detector
+          setState(() {
+            // Função que notifica ao Flutter que ele deve atualizar o estado do widget, é recomendável colocar dentro desta função, apenas o que vai acontecer pra atualizar efetivamente o estado do widget
+            count++; // Aumenta o contador
+          });
+        },
       ),
     );
   }
