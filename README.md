77581035750bd016493d831147c72b8dc7770f21
samurollie <53665491+samurollie@users.noreply.github.com>
2020-12-29 03:16:38 -0300

    Stateful Widget

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Diferente de outros Frameworks, como o Ionic, o Flutter utiliza apenas o Dart, s
 
 Uma tela no Flutter é composta pelo o que são chamados de "Widgets", que são os componentes de uma tela. Tudo na tela é um Widget, desde um botão de pesquisa a um texto no título de uma tela. A partir disso, o Flutter divide os widgets em dois tipos: Stateless Widgets e Stateful Widget.
 
-## Widgets
+## Tipos de Widgets
 
 Ao criar uma página podemos utilizar Stateful Widgets (Widgets com Estado) e Stateless Widgets (Widgets sem estado).
 
@@ -18,10 +18,18 @@ Ao criar uma página podemos utilizar Stateful Widgets (Widgets com Estado) e St
 
 Stateless widgets são uma forma mais simples de widget, são utilizadas para coisas estáticas, que não irão ser atualizadas em uma página, como por exemplo o título.
 
-(adicionar mais informações aq ou fazer uma nova sessão?)
-
 ### Statefull Widget
 
-Statefull Widgets são utilizados para coisas em uma página que serão atualizadas, como por exemplo um texto que mostre qual a sua distancia ate um certo ponto
+Statefull Widgets são utilizados para coisas em uma página que serão atualizadas, como por exemplo um texto que mostre qual a sua distancia ate um certo ponto.
 
-(adicionar mais informações aq ou fazer uma nova sessão?)
+## Funcionamento dos widgets
+
+Os widgets no Flutter respeitam um sistema de hierarquia, onde um widget pode ficar dentro de outro, que fica dentro que outro, que fica dentro de outro...
+
+Com isso, um widget que for filho (dentro de outro widget) irá herdar por padrão as propriedades do seu pai a não ser que seja configurado o contrário. Por exemplo, se um widget tiver um texto azul, os seus filhos também terão um texto azul até que se diga o contrário.
+
+## Widgets Úteis
+
+### Container
+
+Container são widgets genéricos. Tentam por padrão seguir o alinhamento, tamanho e etc do widget filho. Se não tiver nenhum filho, ou essas propriedades estiverem faltando, o container irá ser redenrizado no widget pai com o menor tamanho possível.
--- lib/main.dart
+++ lib/main.dart
@@ -3,11 +3,65 @@ import 'package:flutter/material.dart'; // Biblioteca necessária para utilizar 
 
 main() {
   // Função principal, do dart.
-  runApp(AppWidget(
+  /* runApp(AppWidget(
     title: 'Barbosa',
-  )); // Função do Flutter para rodar o app
+  )); // Função do Flutter para rodar o app */
+
+  runApp(AppWidget());
 }
 
+class AppWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      // O Material App é um widget tipo o Container, porém ele já possui todo o design material, que é o padrão do Google;
+      theme: ThemeData(
+        // Theme é a propriedade do Material app onde eu digo como vai ser o tem o tema do meu app estou passando como valor um widget contendo todas as informações de estilo, nesse caso ThemeData
+        primarySwatch: Colors.blue,
+        // PrimarySwatch diz basicamente qual será a cor dominante do meu aplicativo, dessa forma coisas como barra superior e botões já terão automaticamente esse cor. Estou passando como valor um objeto, Colors.blue
+      ),
+      home:
+          HomePage(), // Home é o atributo do material app que diz que trata como vai ser a página em si do App. Estou passando como valor HomePage() (veja abaixo)
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  // StateFul Widget
+  @override
+  _HomePageState createState() =>
+      _HomePageState(); // Como em um stateful widget ele pode ser atualizado, ele retorna o estado daquele widget.
+}
+
+class _HomePageState extends State<HomePage> {
+  // Configuração da homePage em si
+  int count = 0; // Contador
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      //Estou retornando um container, um widget básico
+      child: Center(
+        // Centraliza o que estiver dentro dele
+        child: GestureDetector(
+          // O widget GestureDetector é um widget que detecta gestos
+          child: Text('Samuel Contador: $count'), // O texto
+          onTap: () {
+            // OnTap é um atributo do widget GestureDetector, tudo que está dentro dele será executado toda vez q alguem tocar no widget
+            print('clicado'); //Imprime clicado no terminal (DART)
+            setState(() {
+              // Função que notifica ao Flutter que ele deve atualizar o estado do widget, é recomendável colocar dentro desta função, apenas o que vai acontecer pra atualizar efetivamente o estado do widget
+              count++; // Aumenta o contador
+            });
+            print('count: $count'); // Imprime o contador no terminal
+          },
+        ),
+      ),
+    );
+  }
+}
+
+/* 
 class AppWidget extends StatelessWidget {
   // Widget com a tela inicial do app
 
@@ -35,5 +89,5 @@ class AppWidget extends StatelessWidget {
         ),
       ),
     );
-  }
-}
+  } 
+}*/
